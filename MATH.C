2a8d1ca79e174df48409a856351644bbe96cfbb9
paulprakash1920 <39926451-paulprakash1920@users.noreply.replit.com>
2025-02-23 12:04:16 +0000

    User checkpoint: Fix: Correct scanf format specifier in MATH.C

--- MATH.C
+++ MATH.C
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
   float s,u,t,a;
-  scanf("%f %f %f",&u,&t&a);
+  scanf("%f %f %f", &u, &t, &a);
   printf("Initial velocity is:%f\n",u);
   printf("The Timing is:%f\n",t);
   printf("The Acceleration is:%f\n",a);
