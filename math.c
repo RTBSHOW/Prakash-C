68c9d4391a077f79502b8df7bc2887910d1698ee
paulprakash1920 <39926451-paulprakash1920@users.noreply.replit.com>
2025-02-23 12:36:36 +0000

    User checkpoint: Add program to calculate the surface area of a sphere and correct typos in existing velocity calculation program.

--- math.c
+++ math.c
@@ -1,15 +1,15 @@
+
 #include <stdio.h>
 int main() {
   float s, u, t, a;
-  printf("Entrt the initial velocity:");
-  scanf("%f",&u);
+  printf("Enter the initial velocity:");
+  scanf("%f", &u);
   printf("Enter the time:");
-  scanf(("%f",&t);
+  scanf("%f", &t);
   printf("Enter the acceleration:");
-  scanf("%f",&a);
-  
+  scanf("%f", &a);
 
   s = u * t + .5 * a * t * t;
-  printf("The direction is:%f", s);
+  printf("The direction is:%f\n", s);
   return 0;
 }
---
+++ sphere.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  float A, pi, r;
+  pi =3.14;
+  printf("Enter the redius:");
+  scanf("%f",&r);
+  A=48pi*r*r;
+  printf("The area is:%f",&A);
+  return 0;
+}
