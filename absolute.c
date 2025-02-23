76d55344dd2a30b0f7dee21aad2d7d1b921a9f40
paulprakash1920 <39926451-paulprakash1920@users.noreply.replit.com>
2025-02-23 14:17:17 +0000

    User checkpoint: Fix: Correct scanf and printf formatting in absolute value calculation

--- absolute.c
+++ absolute.c
@@ -2,10 +2,10 @@
 int main(){
 
 int x;
-  printf(" Enter A value");
-  scanf("%d",x);
-  if(x>=0) { printf("Absolute value is:%d,x");
-          }
+  printf(" Enter a value");
+  scanf("%d",&x);
+  if(x>=0) printf("Absolute value is:%d",x);
+          
   if(x<0){
     int x=x*(-1);
     printf("Absolute value is :%d",x);  }
